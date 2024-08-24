20d2d8bcb28b83f116f2aac2e074635f93ee919f
kevinx <xwk0911@126.com>
2024-08-24 09:00:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# CStar IDE
+
+
+
+## Functionality
+
+This CStar IDE works for */.cst file. It has the following language features:
+- [Hover](#hover)
+- [Show Post](#show-post)
+- [Hide Post](#hide-post)
+- [Start Hol-Lite Server](#start-hol-lite-server)
+- [Stop Hol-Lite Server](#stop-hol-lite-server)
+
+
+## Structure
+
+```
+.
+├── client // Language Client
+│   ├── src
+│   │   ├── test // End to End tests for Language Client / Server
+│   │   └── extension.ts // Language Client entry point
+├── package.json // The extension manifest.
+└── server // Dumped Language Server
+    └── src
+        └── server.ts // Language Server entry point
+```
+
+## Running the CStar IDE
+
+- Run `npm install` in this folder. This installs all necessary npm modules in both the client and server folder
+- Open VS Code on this folder.
+- Press Ctrl+Shift+B to start compiling the client and server in [watch mode](https://code.visualstudio.com/docs/editor/tasks#:~:text=The%20first%20entry%20executes,the%20HelloWorld.js%20file.).
+- Switch to the Run and Debug View in the Sidebar (Ctrl+Shift+D).
+- Select `Launch Client` from the drop down (if it is not already).
+- Press ▷ to run the launch config (F5).
+- In the [Extension Development Host](https://code.visualstudio.com/api/get-started/your-first-extension#:~:text=Then%2C%20inside%20the%20editor%2C%20press%20F5.%20This%20will%20compile%20and%20run%20the%20extension%20in%20a%20new%20Extension%20Development%20Host%20window.) instance of VSCode, open a document in 'plain text' language mode.
+  - Type `j` or `t` to see `Javascript` and `TypeScript` completion.
+  - Enter text content such as `AAA aaa BBB`. The extension will emit diagnostics for all words in all-uppercase.
+
+## Enjoy the Features
+
+### Preparatory Work
+- LSP-Sample Repo
+  - Fix your configuration in package.json (TODO: ondidchangeconfig)
+    - cstaride.usedocker
+    - cstaride.path
+    - cstaride.bdwgcflag
+- Hol-Lite Repo
+  - lsp Branch
+  - due to the error of t_of_yojson, please ensure that package.json and action.ml are consistent.
+    - usedocker
+    ```sh
+    line 9: let usedocker = ref UseDocker.Yes
+    ```
+    - serverroot
+    ```sh
+    line 11: let serverroot = ref {HolLitePath.path = "/mnt/d/ZGC_Lab/hol-lite/"}
+    ```
+    - bdwgcflag
+    ```sh
+    line 13: let bdwgcflag = ref {BDWGCFlag.flags = "-lgc -lpthread -ldl"}
+    ```
+- Press ▷ to run the launch config (F5).
+### Start Hol-Lite Server
+- Press `right-hand button` to see `start hol-lite server in docker`.
+- Check the Server:
+```sh
+docker logs cstar
+```
+
+![logs](./assest/logs.png "logs")
+
+### Stop Hol-Lite Server
+- Press `right-hand button` to see `stop hol-lite server`
+
+### Hover
+Hover `Theorem Value` :
+- make sure the [Hol-Lite Server](#start-hol-lite-server) is running.
+- open your .csv file.
+- save the file, and wait for the fllowing things:
+  - cstaride.path/cstar_output:
+    - FILE_NAME.output.c
+    - FILE_NAME
+    - FILE_NAME_log.csv
+  - Update LexCache (almost in an instant)
+- Move your cursor to the `Theorem Value`.
+
+![logs](./assest/hover.png "logs")
+
+### Show Post
+- Press `right-hand button` to see `CStar Show Post Here`, or
+- Press `Alt + Right`
+
+### Hide Post
+- Press `right-hand button` to see `CStar Hide Post Once`, or
+- Press `Alt + Left`
+
+Please delete the left blank lines by yourself.
+
+
---
+++ client/src/extension.ts
@@ -0,0 +1,165 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { workspace, ExtensionContext, Position } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+import { exec, execSync, spawn  } from 'child_process';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// The server is implemented in node
+	const serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+		}
+	};
+
+	// CStar
+	const config = workspace.getConfiguration('cstaride');
+	const lsppath = config.get<string>('path');
+	const executable = {command: lsppath + "cstarc/_build/default/cstarlsp/cstarlsp.exe" };
+	const cstarserverOptions: ServerOptions = {
+		run: executable,
+		debug: executable,
+	};
+	
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: '*' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		},
+		initializationOptions: config
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'cstar',
+		'CStar IDE',
+		cstarserverOptions,
+		clientOptions
+	);
+	// activeTextEditor
+	let editor = vscode.window.activeTextEditor;
+
+	// render decoration
+	const backgroundColor = new vscode.ThemeColor('cstar.inlineGhostBackgroundColor');
+	const color = new vscode.ThemeColor('cstar.inlineGhostForegroundColor');
+	const ghostlines = ["Hello CStar!", "Hello VST-A!"];
+	let decorations: vscode.DecorationOptions[] = []; // post lines, recreate everytime
+	const decorationType_stack: vscode.TextEditorDecorationType[] = []; // stack
+
+	// registerCommand showpost
+	context.subscriptions.push(vscode.commands.registerCommand('cstar.showpost', function () {
+		vscode.window.showInformationMessage('Hello CStar!');
+		// const editor = vscode.window.activeTextEditor;
+		if(!editor) return;
+		client.sendRequest("cstar/showpost", {document: editor.document.getText(), uri: editor.document.uri.path, line: editor.selection.end.line + 1 });
+		// create decoration
+		render();
+	}));
+
+	// registerCommand hidepost
+	context.subscriptions.push(vscode.commands.registerCommand('cstar.hidepost', () => {
+		const decorationType_top = decorationType_stack.pop();
+		if (decorationType_top) {
+			decorationType_top.dispose(); 
+			editor.setDecorations(decorationType_top, decorations);
+		}
+	}));
+
+	// registerCommand starthollite
+	context.subscriptions.push(vscode.commands.registerCommand('cstar.starthollite', () => {
+		exec("docker run --rm -v " + lsppath + ":/hol-lite --name cstar wybxc/hol-lite /bin/bash -c 'cd /hol-lite && make run' tail -f /dev/null > /dev/null 2>&1");
+	}));
+	// docker run --rm -v /mnt/d/ZGC_Lab/hol-lite/:/hol-lite --name cstar wybxc/hol-lite /bin/bash -c 'cd /hol-lite && make run' tail -f /dev/null > /dev/null 2>&1
+	// docker logs cstar
+
+	// registerCommand stophollite
+	context.subscriptions.push(vscode.commands.registerCommand('cstar.stophollite', () => {
+		const output = exec("docker stop cstar");
+	}));
+
+	// render function
+	function render() {
+		// init
+		decorations = [];
+		let curlineofghost = 0;
+		const startline = editor.selection.end.line + 1;
+		const cstarlines = editor.document.getText().split('\n');
+		const decorationType = vscode.window.createTextEditorDecorationType({
+			after: {
+				backgroundColor,
+				color,
+				fontStyle: "italic",
+			}
+		});
+		decorationType_stack.push(decorationType);
+
+		// insert blank lines
+		for (let i = 0; i < ghostlines.length; i++) {
+			cstarlines.splice(startline + i, 0, '');
+		}
+		const newText = cstarlines.join('\n');
+		editor.edit((editBuilder) => {
+			editBuilder.replace(new vscode.Range(0, 0, cstarlines.length, 0), newText);
+		});
+		editor.document.save();
+
+		// insert ghostlines
+		for (const line of ghostlines) {
+			const contentText = "▷ " + line;
+			decorations.push({
+				range: new vscode.Range(startline + curlineofghost, 0, startline + curlineofghost, 0),
+				renderOptions: {
+					after: {
+						contentText
+					},
+				}
+			});
+			curlineofghost += 1;
+		}
+		editor.setDecorations(decorationType, decorations);
+	}
+
+	// change editor
+	vscode.window.onDidChangeActiveTextEditor(neweditor => {
+		editor = neweditor;
+		// keep decorations around file
+		// if (editor) {render();}
+		// delete blank lines
+		// sendRequest("find blank lines in functions",editor.document).then(delete(result));
+	}, null, context.subscriptions);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
