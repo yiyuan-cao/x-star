7048b6e3804793e7c1dae3d6c9029965dcbd7199
solarisch <oier_chenhoujin@outlook.com>
2024-09-11 04:48:26 +0000

    feat: syntax highlighting

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -41,7 +41,7 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: '*' }],
+		documentSelector: [{ scheme: 'file', language: 'cstar' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
