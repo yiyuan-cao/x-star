23fb9a063cdb0329188e4bddd5b29e3b83f3337c
mgxzjjyb <2207769089@qq.com>
2024-10-14 13:35:52 +0800

    add annotation in attach.c

---
+++ examples/buddy_allocator/attach.c
@@ -0,0 +1,380 @@
+static void __hyp_attach_page(struct hyp_pool *pool, struct hyp_page *p)
+/*
+PARAMETERS:
+	l : ((num#num)#(PTR#PTR))list
+	dl : ((PTR#PTR)list)list
+	hl : ((PTR#PTR)list)list
+	pid : num
+	pi : num
+
+	[hided] start : num
+	[hided] end : num
+	[hided] max_order : num
+	[hided] offset : num
+	[hided] len : num
+
+	[hided] 
+		(start < end) && (end * PAGE_SIZE < PAGE_LIM) /\
+		(max_order <= MAX_ORDER) /\
+		(offset <= start * PAGE_SIZE) /\
+		(PAGESIZE divides offset) /\
+		(len = end - start)
+
+REQUIRE:
+	pool_const pool **
+	dlist_head_repr pool 0 max_order hl **
+	free_area_repr (ifilter l) start end l **
+    free_area_head_repr (ifilter l) start end dl **
+	store_pageinfo_array __hyp_vmemmap start pid (take l pi) **
+	(
+		DATA_AT... p (0, ORD (nth l pi))
+		&& PURE (
+			(~((ORD (nth l pi)) = NO_ORDER) ==> 
+				(ORD (nth l pi) < max_order) /\ 
+				((2 EXP (ORD (nth l pi))) divides pid)) /\ 
+			(REF (nth l pi) < REF_LIM)
+	)) **
+	store_pageinfo_array __hyp_vmemmap (SUC pid) end (rest l (SUC pi)) **
+	store_undef_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP (ORD (nth l pi)))) (PAGE_SIZE * (2 EXP (ORD (nth l pi))))
+	&& PURE (
+		dlist_node pool_ptr (ifilter l) l dl hl start end dl /\
+		dlist_head pool_ptr l dl 0 max_order hl /\
+		(LENGTH l = len) /\
+		(LENGTH dl = len) /\
+		(LENGTH hl = max_order) /\
+		(p = __hyp_vmemmap + pi * HYP_PAGE_SIZE) /\
+		(pi < len) /\
+		(pid = start + pi) /\
+		~((ORD (nth l pi)) = NO_ORDER) /\
+		(REF (nth l pi) > 0)
+	)
+
+ENSURE: (i.e. total_repr pool __hyp_vmemmap l' dl' hl')
+	pool_const pool **
+	dlist_head_repr pool 0 max_order hl **
+	free_area_repr (ifilter l') start end l' **
+    free_area_head_repr (ifilter l') start end dl' **
+	store_pageinfo_array __hyp_vmemmap start end l' **
+	&& PURE (
+		dlist_node pool_ptr (ifilter l') l' dl' hl' start end dl' /\
+		dlist_head pool_ptr l' dl' 0 max_order hl' /\
+		(LENGTH l' = len)
+		(LENGTH dl' = len) /\
+		(LENGTH hl' = max_order)
+	)
+*/
+{
+// put PURE () together
+
+	struct hyp_page *buddy = NULL;
+// get: buddy = NULL
+
+// from: DATA_AT... p (0, ORD (nth l pi))
+	u8 order = p->order;
+// get: order = ORD (nth l pi)
+// rewrite ORD (nth l pi) to order
+
+// from: store_undef_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order))
+	memset(__cerbvar_copy_alloc_id(hyp_page_to_virt(p), cn_virt_base), 0, PAGE_SIZE << order);
+// to: store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order))
+
+// from: DATA_AT... p (0, order)
+	p->order = HYP_NO_ORDER;
+// to: DATA_AT... p (0, NO_ORDER)
+
+/*
+NOW:
+	pool_const pool **
+	dlist_head_repr pool 0 max_order hl **
+	free_area_repr (ifilter l) start end l **
+    free_area_head_repr (ifilter l) start end dl **
+	store_pageinfo_array __hyp_vmemmap start pid (take l pi) **
+	DATA_AT... p (0, NO_ORDER) **
+	store_pageinfo_array __hyp_vmemmap (SUC pid) end (rest l (SUC pi)) **
+	store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order))
+	&& PURE (
+		dlist_node pool_ptr (ifilter l) l dl hl start end dl /\
+		dlist_head pool_ptr l dl 0 max_order hl /\
+		(LENGTH l = len) /\
+		(LENGTH dl = len) /\
+		(LENGTH hl = max_order) /\
+		(p = __hyp_vmemmap + pi * HYP_PAGE_SIZE) /\
+		(pi < len) /\
+		(pid = start + pi) /\
+		~(order = NO_ORDER) /\
+		(REF (nth l pi) > 0) /\
+		(~(order = NO_ORDER) ==> 
+			(order < max_order) /\ 
+			((2 EXP order) divides pid)) /\ 
+		(REF (nth l pi) < REF_LIM) /\
+		(buddy = NULL) /\
+		(order = ORD (nth l pi))
+	)
+*/
+
+/*
+ENTAILMENTS:
+1.	REF (nth l pi) > 0 ==>
+	ifilter l = ifilter (modified l pi (0, NO_ORDER))
+2.	filter pi = F ==>
+	free_area_repr filter start end l = 
+	free_area_repr filter start end (modified l pi v)
+3.	apply merge_modified_list_lemma on store_pageinfo_array
+4.	REF (nth l pi) > 0 \/ ORD (nth l pi) = NO_ORDER ==>
+	dlist_node pool_ptr filter l hl start end dl =
+	dlist_node pool_ptr filter (modified l pi v) hl start end dl
+5.	REF (nth l pi) > 0 \/ ORD (nth l pi) = NO_ORDER ==>
+	dlist_head pool_ptr l 0 max_order hl =
+	dlist_head pool_ptr (modified l pi v) 0 max_order hl
+6.	LENGTH l = LENGTH (modified l pi v)
+
+inv_l := modified l pi (0, NO_ORDER)
+inv_dl := dl
+inv_hl := hl
+*/
+
+/*
+INVARIANT: 
+	pool_const pool **
+	dlist_head_repr pool 0 max_order inv_hl **
+	free_area_repr (ifilter inv_l) start end inv_l **
+    free_area_head_repr (ifilter inv_l) start end inv_dl **
+	store_pageinfo_array __hyp_vmemmap start end inv_l **
+	store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order)) **
+	&& PURE (
+		dlist_node pool_ptr (ifilter inv_l) inv_l inv_dl inv_hl start end inv_dl /\
+		dlist_head pool_ptr inv_l inv_dl 0 max_order inv_hl /\
+		(LENGTH inv_l = len) /\
+		(LENGTH inv_dl = len) /\
+		(LENGTH inv_hl = max_order) /\
+		(p = __hyp_vmemmap + pi * HYP_PAGE_SIZE) /\
+		(pi < len) /\
+		(pid = start + pi) /\
+		(order + 1 < max_order) /\
+		((2 EXP order) divides pid) /\
+		(REF (nth inv_l pi) = 0) /\
+		(ORD (nth inv_l pi) = NO_ORDER)
+	)
+*/
+	for (; (order + 1) < pool->max_order; order++)
+	{
+// from: p = __hyp_vmemmap + pi * HYP_PAGE_SIZE
+//		 pid = start + pi
+//		 (2 EXP order) divides pid
+		buddy = __find_buddy_avail(pool, p, order);
+		if (!buddy)
+			break;
+// get: buddy = __hyp_vmemmap + bi * HYP_PAGE_SIZE
+//		bi < len
+//		bid = start + bi
+//		REF (nth inv_l bi) = 0
+//		ORD (nth inv_l bi) = order
+//		(2 EXP (SUC order)) divides (min pid bid)
+
+// from: dlist_head_repr pool 0 max_order inv_hl
+//		dlist_node pool_ptr (ifilter inv_l) inv_l inv_dl inv_hl start end inv_dl
+//		dlist_head pool_ptr inv_l inv_dl 0 max_order inv_hl
+//		free_area_head_repr (ifilter inv_l) start end inv_dl
+// 		buddy = __hyp_vmemmap + bi * HYP_PAGE_SIZE
+//		bi < len
+//		bid = start + bi
+//		REF (nth inv_l bi) = 0
+//		ORD (nth inv_l bi) = order
+//		order + 1 < max_order
+		page_remove_from_list_pool(pool, buddy);
+// to: dlist_head_repr pool 0 max_order new_hl
+//		dlist_node pool_ptr (ifilter new_l) inv_l new_dl new_hl start end new_dl
+//		dlist_head pool_ptr inv_l new_dl 0 max_order new_hl
+//		free_area_head_repr (ifilter new_l) start end new_dl **
+//		DATA_AT_PTR (id2vi (&bid), &0) **
+//		DATA_AT_PTR (id2vi (&bid) + &PTR_SIZE, &0)
+// new_l := modified inv_l bi (0, NO_ORDER)
+
+// apply lemma4 lemma5
+// get: dlist_node pool_ptr (ifilter new_l) new_l new_dl new_hl start end new_dl
+//		dlist_head pool_ptr new_l new_dl 0 max_order new_hl
+
+// apply merge_modified_list_lemma on store_pageinfo_array
+// from: store_pageinfo_array __hyp_vmemmap start end inv_l
+// to: store_pageinfo_array __hyp_vmemmap start bid (take l bi) **
+// 		(
+// 			DATA_AT... buddy (0, order)
+// 			&& PURE (
+// 				(~(order = NO_ORDER) ==> 
+// 					(order < max_order) /\ 
+// 					((2 EXP order) divides bid)) /\ 
+// 				(0 < REF_LIM)
+// 		)) **
+// 		store_pageinfo_array __hyp_vmemmap (SUC bid) end (rest l (SUC bi))
+
+// from: DATA_AT... buddy (0, order)
+		buddy->order = HYP_NO_ORDER;
+// to: DATA_AT... buddy (0, NO_ORDER)
+
+// from: (2 EXP (SUC order)) divides (min pid bid)
+		p = min(p, buddy);
+// get: (2 EXP (SUC order)) divides new_pid
+
+/*
+NOW:
+	pool_const pool **
+	dlist_head_repr pool 0 max_order new_hl **
+	free_area_repr (ifilter inv_l) start end inv_l **
+    free_area_head_repr (ifilter new_l) start end new_dl **
+	DATA_AT_PTR (id2vi (&bid), &0) **
+	DATA_AT_PTR (id2vi (&bid) + &PTR_SIZE, &0) **
+	store_pageinfo_array __hyp_vmemmap start bid (take l bi) **
+	(
+		DATA_AT... buddy (0, NO_ORDER)
+		&& PURE (
+			(~(order = NO_ORDER) ==> 
+				(order < max_order) /\ 
+				((2 EXP order) divides bid)) /\ 
+			(0 < REF_LIM)
+ 	)) **
+ 	store_pageinfo_array __hyp_vmemmap (SUC bid) end (rest l (SUC bi)) **
+	store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order)) **
+	&& PURE (
+		dlist_node pool_ptr (ifilter new_l) new_l new_dl new_hl start end new_dl /\
+		dlist_head pool_ptr new_l new_dl 0 max_order new_hl /\
+		(LENGTH inv_l = len) /\
+		(LENGTH inv_dl = len) /\
+		(LENGTH inv_hl = max_order) /\
+		(p = __hyp_vmemmap + new_pi * HYP_PAGE_SIZE) /\
+		(new_pi < len) /\
+		(new_pid = start + new_pi) /\
+		(order + 1 < max_order) /\
+		((2 EXP (SUC order)) divides new_pid) /\
+		(buddy = __hyp_vmemmap + bi * HYP_PAGE_SIZE) /\
+		(bi < len) /\
+		(bid = start + bi) /\
+		(REF (nth inv_l bi) = 0) /\
+		(ORD (nth inv_l bi) = order) /\
+		(REF (nth new_l bi) = 0) /\
+		(ORD (nth new_l bi) = NO_ORDER) /\
+		(REF (nth inv_l pi) = 0) /\
+		(ORD (nth inv_l pi) = NO_ORDER)
+	)
+
+new_l := modified inv_l bi (0, NO_ORDER)	
+*/
+
+/*
+ENTAILMENTS:
+1.	ifilter l bid = T ==>
+	free_area_repr (ifilter l) start end l =
+	free_area_repr (ifilter (modified inv_l bi (0, NO_ORDER))) start end l **
+	store_zero_array (id2vi (&bid)) (PTR_SIZE * 2) (PAGE_SIZE * (2 EXP (ORD (nth l bi)))) (PAGE_SIZE * (2 EXP (ORD (nth l bi))) - PAGE_SIZE * 2)
+2.	filter bid = F ==>
+	free_area_repr filter start end l =
+	free_area_repr filter start end l (modified inv_l bi v)
+3.	apply merge_modified_list_lemma on store_pageinfo_array
+4.	DATA_AT_PTR (id2vi (&bid), &0) **
+	DATA_AT_PTR (id2vi (&bid) + &PTR_SIZE, &0) **
+	store_zero_array (id2vi (&bid)) (PTR_SIZE * 2) (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order) - PAGE_SIZE * 2) =
+	store_zero_array (id2vi (&bid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order))
+5.	new_pid = min pid bid /\ ... ==>
+	store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order)) **
+	store_zero_array (id2vi (&bid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order)) =
+	store_zero_array (id2vi (&new_pid)) 0 (PAGE_SIZE * (2 EXP (SUC order))) (PAGE_SIZE * (2 EXP (SUC order)))
+*/
+	}
+/*
+NOW:
+	pool_const pool **
+	dlist_head_repr pool 0 max_order hl **
+	free_area_repr (ifilter l) start end l **
+    free_area_head_repr (ifilter l) start end dl **
+	store_pageinfo_array __hyp_vmemmap start end l **
+	store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order)) **
+	&& PURE (
+		dlist_node pool_ptr (ifilter l) l dl hl start end dl /\
+		dlist_head pool_ptr l dl 0 max_order hl /\
+		(LENGTH l = len) /\
+		(LENGTH dl = len) /\
+		(LENGTH hl = max_order) /\
+		(p = __hyp_vmemmap + pi * HYP_PAGE_SIZE) /\
+		(pi < len) /\
+		(pid = start + pi) /\
+		(order + 1 < max_order) /\
+		((2 EXP order) divides pid) /\
+		(REF (nth l pi) = 0) /\
+		(ORD (nth l pi) = NO_ORDER)
+	)
+*/
+	p->order = order;
+/*
+NOW:
+	pool_const pool **
+	dlist_head_repr pool 0 max_order hl **
+	free_area_repr (ifilter l) start end l **
+    free_area_head_repr (ifilter l) start end dl **
+	store_pageinfo_array __hyp_vmemmap start end (modified l pi (0, order)) **
+	store_zero_array (id2vi (&pid)) 0 (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order)) **
+	&& PURE (
+		dlist_node pool_ptr (ifilter l) l dl hl start end dl /\
+		dlist_head pool_ptr l dl 0 max_order hl /\
+		(LENGTH l = len) /\
+		(LENGTH dl = len) /\
+		(LENGTH hl = max_order) /\
+		(p = __hyp_vmemmap + pi * HYP_PAGE_SIZE) /\
+		(pi < len) /\
+		(pid = start + pi) /\
+		(order + 1 < max_order) /\
+		((2 EXP order) divides pid) /\
+		(REF (nth l pi) = 0) /\
+		(ORD (nth l pi) = NO_ORDER)
+	)
+*/
+
+// from: dlist_head_repr pool 0 max_order hl
+//		dlist_node pool_ptr (ifilter l) l dl hl start end dl
+//		dlist_head pool_ptr l dl 0 max_order hl
+//		free_area_head_repr (ifilter l) start end dl **
+//		DATA_AT_PTR (id2vi (&pid), &0) **
+//		DATA_AT_PTR (id2vi (&pid) + &PTR_SIZE, &0)
+// 		p = __hyp_vmemmap + pi * HYP_PAGE_SIZE
+//		pi < len
+//		pid = start + pi
+//		REF (nth l pi) = 0
+//		ORD (nth l pi) = NO_ORDER
+	page_add_to_list_pool(pool, p, &pool->free_area[order]);
+// to: dlist_head_repr pool 0 max_order new_hl
+//		dlist_node pool_ptr (ifilter new_l) new_l new_dl new_hl start end new_dl
+//		dlist_head pool_ptr new_l new_dl 0 max_order new_hl
+//		free_area_head_repr (ifilter new_l) start end new_dl
+// new_l := modified l pi (0, order)
+
+/*
+NOW:
+	pool_const pool **
+	dlist_head_repr pool 0 max_order new_hl **
+	free_area_repr (ifilter l) start end l **
+    free_area_head_repr (ifilter new_l) start end new_dl **
+	store_pageinfo_array __hyp_vmemmap start end new_l **
+	store_zero_array (id2vi (&pid)) (PAGE_SIZE * 2) (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order) - PAGE_SIZE * 2) **
+	&& PURE (
+		dlist_node pool_ptr (ifilter new_l) new_l new_dl new_hl start end new_dl /\
+		dlist_head pool_ptr new_l new_dl 0 max_order new_hl /\
+		(LENGTH l = len) /\
+		(LENGTH dl = len) /\
+		(LENGTH hl = max_order) /\
+		(p = __hyp_vmemmap + pi * HYP_PAGE_SIZE) /\
+		(pi < len) /\
+		(pid = start + pi) /\
+		(order + 1 < max_order) /\
+		((2 EXP order) divides pid) /\
+		(REF (nth l pi) = 0) /\
+		(ORD (nth l pi) = NO_ORDER)
+	)
+*/
+
+/*
+ENTAILMENT:
+1.	(REF (nth l pi) = 0) /\ ORD (nth l pi) = NO_ORDER) ==>
+	free_area_repr (ifilter l) start end l **
+	store_zero_array (id2vi (&pid)) (PAGE_SIZE * 2) (PAGE_SIZE * (2 EXP order)) (PAGE_SIZE * (2 EXP order) - PAGE_SIZE * 2) =
+	free_area_repr (ifilter (modified l pi (0, order))) start end (modified l pi (0, order))
+*/
+}
+	
--- examples/buddy_allocator/def.ml
+++ examples/buddy_allocator/def.ml
@@ -18,15 +18,15 @@ let PH2VI_DEF = define `ph2vi (ph : PTR) = ph - &offset`;;
 let VI2PH_DEF = define `vi2ph (vi : PTR) = vi + &offset`;;
 let PH2ID_DEF = define `ph2id (ph : PTR) = num_of_int (ph div &PAGE_SIZE)`;;
 let VI2ID_DEF = define `vi2id (vi : PTR) = ph2id (vi2ph vi)`;;
-let ID2PH_DEF = define `id2ph (id : int) = id * &PAGE_SIZE`;;
-let ID2VI_DEF = define `id2vi (id : int) = ph2vi (id2ph id)`;;
+let ID2PH_DEF = define `id2ph (id : num) = &(id * PAGE_SIZE)`;;
+let ID2VI_DEF = define `id2vi (id : num) = ph2vi (id2ph id)`;;
+let ID2I_DEF = define `id2i (id : num) = id - start`;;
+let VI2I_DEF = define `vi2i (vi : PTR) = id2i (vi2id vi)`;;
 
-(* let TUPLE_DEF = define_type "info = INFO (num#num)#(PTR#PTR)";; *)
-
-let REF_DEF = define `REF (i : (num#num)#(PTR#PTR)) = FST (FST i)`;;
-let ORD_DEF = define `ORD (i : (num#num)#(PTR#PTR)) = SND (FST i)`;;
-let NXT_DEF = define `NXT (i : (num#num)#(PTR#PTR)) = FST (SND i)`;;
-let PRV_DEF = define `PRV (i : (num#num)#(PTR#PTR)) = SND (SND i)`;;
+let REF_DEF = define `REF (i : (num#num)) = FST i`;;
+let ORD_DEF = define `ORD (i : (num#num)) = SND i`;;
+let NXT_DEF = define `NXT (i : (PTR#PTR)) = FST i`;;
+let PRV_DEF = define `PRV (i : (PTR#PTR)) = SND i`;;
 
 let STORE_UNDEF_ARRAY_DEF = define `
     (! (addr : PTR) (lo : num) (hi : num). 
@@ -46,19 +46,19 @@ let STORE_ZERO_ARRAY_DEF = define `
 
 let STORE_PAGEINFO_ARRAY_DEF = define `
     (! (addr : PTR) (lo : num) (hi : num).
-    store_pageinfo_array addr lo hi (NIL : ((num#num)#(PTR#PTR))list) = (PURE (lo = hi) SEPAND EMP)) /\
-    (! (addr : PTR) (lo : num) (hi : num) (h : (num#num)#(PTR#PTR)) (t : ((num#num)#(PTR#PTR))list).
+    store_pageinfo_array addr lo hi (NIL : (num#num)list) = (PURE (lo = hi) SEPAND EMP)) /\
+    (! (addr : PTR) (lo : num) (hi : num) (h : (num#num)) (t : (num#num)list).
     store_pageinfo_array addr lo hi (CONS h t) = ((
-            PURE (((~((ORD h) = NO_ORDER) ==> (ORD h <= max_order) /\ ((2 EXP (ORD h)) divides lo))) /\ (REF h < REF_LIM)) SEPAND
-            DATA_AT_SHORT (addr + &((lo - start) * 4), &(REF h)) SEPCONJ
-            DATA_AT_CHAR (addr + &((lo - start) * 4 + 2), &(ORD h)) SEPCONJ
-            DATA_AT_CHAR_ANY (addr + &((lo - start) * 4 + 3))
+            PURE ((~((ORD h) = NO_ORDER) ==> (ORD h < max_order) /\ ((2 EXP (ORD h)) divides lo)) /\ (REF h < REF_LIM)) SEPAND
+            DATA_AT_SHORT (addr + &((id2i lo) * 4), &(REF h)) SEPCONJ
+            DATA_AT_CHAR (addr + &((id2i lo) * 4 + 2), &(ORD h)) SEPCONJ
+            DATA_AT_CHAR_ANY (addr + &((id2i lo) * 4 + 3))
         ) SEPCONJ store_pageinfo_array addr (SUC lo) hi t))`;;
 
 let POOL_CONST_DEF = define `
     pool_const (pool_ptr : PTR) = (
-        DATA_AT_INT (pool_ptr + &(LIST_HEAD_SIZE * MAX_ORDER), id2ph (&start)) SEPCONJ
-        DATA_AT_INT (pool_ptr + &(LIST_HEAD_SIZE * MAX_ORDER + 8), id2ph (&end)) SEPCONJ
+        DATA_AT_INT (pool_ptr + &(LIST_HEAD_SIZE * MAX_ORDER), id2ph start) SEPCONJ
+        DATA_AT_INT (pool_ptr + &(LIST_HEAD_SIZE * MAX_ORDER + 8), id2ph end) SEPCONJ
         DATA_AT_CHAR (pool_ptr + &(LIST_HEAD_SIZE * MAX_ORDER + 16), &max_order))`;;
 
 let DLIST_HEAD_REPR_DEF = define `
@@ -66,8 +66,8 @@ let DLIST_HEAD_REPR_DEF = define `
     dlist_head_repr addr order maxorder (NIL : (PTR#PTR)list) = (PURE (order = maxorder) SEPAND EMP)) /\
     (! (addr : PTR) (order : num) (maxorder : num) (h : PTR#PTR) (t : (PTR#PTR)list).
     dlist_head_repr addr order maxorder (CONS h t) = ((
-        DATA_AT_PTR (addr + &(LIST_HEAD_SIZE * order), FST h) SEPCONJ
-        DATA_AT_PTR (addr + &(LIST_HEAD_SIZE * order + PTR_SIZE), SND h)
+        DATA_AT_PTR (addr + &(LIST_HEAD_SIZE * order), NXT h) SEPCONJ
+        DATA_AT_PTR (addr + &(LIST_HEAD_SIZE * order + PTR_SIZE), PRV h)
     ) SEPCONJ dlist_head_repr addr (SUC order) maxorder t))`;;
 
 let NTH_DEF = define `
@@ -90,57 +90,85 @@ let REST_DEF = define `
 
 (* EQ_EXT *)
 
-let IFILTER_DEF = define `ifilter (l : ((num#num)#(PTR#PTR))list) (i : num) = ((REF (nth l i) = 0) /\ ~(ORD (nth l i) = NO_ORDER))`;;
+let IFILTER_DEF = define `ifilter (l : (num#num)list) (i : num) = ((REF (nth l i) = 0) /\ ~(ORD (nth l i) = NO_ORDER))`;;
 
 let () = new_constant("DATA_AT_PTR_ANY", `: PTR -> HPROP`);;
 
 let FREE_AREA_REPR_DEF = define `
     (! (f : num -> bool) (lo : num) (hi : num).
-    free_area_repr f lo hi (NIL : ((num#num)#(PTR#PTR))list) = (PURE (lo = hi) SEPAND EMP)) /\
-    (! (f : num -> bool) (lo : num) (hi : num) (h : (num#num)#(PTR#PTR)) (t : ((num#num)#(PTR#PTR))list).
+    free_area_repr f lo hi (NIL : (num#num)list) = (PURE (lo = hi) SEPAND EMP)) /\
+    (! (f : num -> bool) (lo : num) (hi : num) (h : num#num) (t : (num#num)list).
     free_area_repr f lo hi (CONS h t) = 
         ((if f lo then
-            DATA_AT_PTR (id2vi (&lo), NXT h) SEPCONJ
-            DATA_AT_PTR (id2vi (&lo) + &PTR_SIZE, PRV h) SEPCONJ
-            (store_zero_array (id2vi (&lo)) (PTR_SIZE * 2) (PAGE_SIZE * (2 EXP (ORD h))) (PAGE_SIZE * (2 EXP (ORD h)) - PAGE_SIZE * 2))
+            (store_zero_array (id2vi lo) (PTR_SIZE * 2) (PAGE_SIZE * (2 EXP (ORD h))) (PAGE_SIZE * (2 EXP (ORD h)) - PAGE_SIZE * 2))
         else EMP) SEPCONJ
         free_area_repr f (SUC lo) hi t))`;;
 
+let FREE_AREA_HEAD_REPR_DEF = define `
+    (! (f : num -> bool) (lo : num) (hi : num).
+    free_area_head_repr f lo hi (NIL : (PTR#PTR)list) = (PURE (lo = hi) SEPAND EMP)) /\
+    (! (f : num -> bool) (lo : num) (hi : num) (h : (PTR#PTR)) (t : (PTR#PTR)list).
+    free_area_head_repr f lo hi (CONS h t) = 
+        ((if f lo then
+            DATA_AT_PTR (id2vi lo, NXT h) SEPCONJ
+            DATA_AT_PTR (id2vi lo + &PTR_SIZE, PRV h)
+        else EMP) SEPCONJ
+        free_area_head_repr f (SUC lo) hi t))`;;
+
 let DLIST_NODE_DEF = define `
-    (! (addr : PTR) (l : ((num#num)#(PTR#PTR))list) (hl : (PTR#PTR)list) (lo : num) (hi : num).
-    dlist_node addr l hl lo hi (NIL : ((num#num)#(PTR#PTR))list) = (lo = hi)) /\
-    (! (addr : PTR) (l : ((num#num)#(PTR#PTR))list) (hl : (PTR#PTR)list) (lo : num) (hi : num) (h : (num#num)#(PTR#PTR)) (t : ((num#num)#(PTR#PTR))list).
-    dlist_node addr l hl lo hi (CONS h t) = ((
-        (((NXT h = addr + &(LIST_HEAD_SIZE * (ORD h))) /\ (SND (nth hl (ORD h)) = id2vi (&lo))) \/
-        ((PRV (nth l (vi2id (NXT h) - start)) = id2vi (&lo)) /\ (REF (nth l (vi2id (NXT h) - start)) = 0) /\ ~(ORD (nth l (vi2id (NXT h) - start)) = NO_ORDER))) /\
-        (((PRV h = addr + &(LIST_HEAD_SIZE * (ORD h))) /\ (FST (nth hl (ORD h)) = id2vi (&lo))) \/
-        ((NXT (nth l (vi2id (PRV h) - start)) = id2vi (&lo)) /\ (REF (nth l (vi2id (PRV h) - start)) = 0) /\ ~(ORD (nth l (vi2id (PRV h) - start)) = NO_ORDER)))
-    ) /\ dlist_node addr l hl (SUC lo) hi t))`;;
+    (! (addr : PTR) (f : num -> bool) (l : (num#num)list) (dl : (PTR#PTR)list) (hl : (PTR#PTR)list) (lo : num) (hi : num).
+    dlist_node addr f l dl hl lo hi (NIL : (PTR#PTR)list) = (lo = hi)) /\
+    (! (addr : PTR) (f : num -> bool) (l : (num#num)list) (dl : (PTR#PTR)list) (hl : (PTR#PTR)list) (lo : num) (hi : num) (h : PTR#PTR) (t : (PTR#PTR)list).
+    dlist_node addr f l dl hl lo hi (CONS h t) = ((
+        if f lo then
+            (((NXT h = addr + &(LIST_HEAD_SIZE * (ORD (nth l (id2i lo))))) 
+                /\ (PRV (nth hl (ORD (nth l (id2i lo)))) = id2vi lo))
+            \/
+            ((PRV (nth dl (vi2i (NXT h))) = id2vi lo) /\ 
+            (REF (nth l (vi2i (NXT h))) = 0) /\ 
+            ~(ORD (nth l (vi2i (NXT h))) = NO_ORDER) /\ 
+            (ORD (nth l (vi2i (NXT h))) = (ORD (nth l (id2i lo)))))) 
+            /\
+            (((PRV h = addr + &(LIST_HEAD_SIZE * (ORD (nth l (id2i lo))))) 
+                /\ (NXT (nth hl (ORD (nth l (id2i lo)))) = id2vi lo))
+            \/
+            ((NXT (nth dl (vi2i (PRV h))) = id2vi lo) /\ 
+            (REF (nth l (vi2i (PRV h))) = 0) /\ 
+            ~(ORD (nth l (vi2i (PRV h))) = NO_ORDER) /\ 
+            (ORD (nth l (vi2i (PRV h))) = (ORD (nth l (id2i lo))))))
+        else T
+    ) /\ dlist_node addr f l dl hl (SUC lo) hi t))`;;
 
 let DLIST_HEAD_DEF = define `
-    (! (addr : PTR) (l : ((num#num)#(PTR#PTR))list) (order : num) (maxorder : num).
-    dlist_head addr l order maxorder (NIL : (PTR#PTR)list) = (order = maxorder)) /\
-    (! (addr : PTR) (l : ((num#num)#(PTR#PTR))list) (order : num) (maxorder : num) (h : PTR#PTR) (t : (PTR#PTR)list).
-    dlist_head addr l order maxorder (CONS h t) = ((
-        ((FST h = addr + &(LIST_HEAD_SIZE * order)) /\ (SND h = addr + &(LIST_HEAD_SIZE * order))) \/
-        ((PRV (nth l (vi2id (FST h) - start)) = addr + &(LIST_HEAD_SIZE * order)) /\ 
-        (REF (nth l (vi2id (FST h) - start)) = 0) /\ 
-        ~(ORD (nth l (vi2id (FST h) - start)) = NO_ORDER) /\
-        (NXT (nth l (vi2id (SND h) - start)) = addr + &(LIST_HEAD_SIZE * order)) /\ 
-        (REF (nth l (vi2id (SND h) - start)) = 0) /\ 
-        ~(ORD (nth l (vi2id (SND h) - start)) = NO_ORDER))
-    ) /\ dlist_head addr l (SUC order) maxorder t))`;;
+    (! (addr : PTR) (l : (num#num)list) (dl : (PTR#PTR)list) (order : num) (maxorder : num).
+    dlist_head addr l dl order maxorder (NIL : (PTR#PTR)list) = (order = maxorder)) /\
+    (! (addr : PTR) (l : (num#num)list) (dl : (PTR#PTR)list) (order : num) (maxorder : num) (h : PTR#PTR) (t : (PTR#PTR)list).
+    dlist_head addr l dl order maxorder (CONS h t) = ((
+        ((NXT h = addr + &(LIST_HEAD_SIZE * order)) /\ (PRV h = addr + &(LIST_HEAD_SIZE * order)))
+        \/
+        ((PRV (nth dl (vi2i (NXT h))) = addr + &(LIST_HEAD_SIZE * order)) /\ 
+        (REF (nth l (vi2i (NXT h))) = 0) /\ 
+        ~(ORD (nth l (vi2i (NXT h))) = NO_ORDER) /\ 
+        (ORD (nth l (vi2i (NXT h))) = order) /\
+        (NXT (nth dl (vi2i (PRV h))) = addr + &(LIST_HEAD_SIZE * order)) /\ 
+        (REF (nth l (vi2i (PRV h))) = 0) /\ 
+        ~(ORD (nth l (vi2i (PRV h))) = NO_ORDER) /\ 
+        (ORD (nth l (vi2i (PRV h))) = order))
+    ) /\ dlist_head addr l dl (SUC order) maxorder t))`;;
 
 let TOTAL_REPR_DEF = define `
-    total_repr (pool_ptr : PTR) (vmemmap_ptr : PTR) (l : ((num#num)#(PTR#PTR))list) (hl : (PTR#PTR)list) = (
+    total_repr (pool_ptr : PTR) (vmemmap_ptr : PTR) (l : (num#num)list) (dl : (PTR#PTR)list) (hl : (PTR#PTR)list) = (
         pool_const pool_ptr SEPCONJ
         dlist_head_repr pool_ptr 0 max_order hl SEPCONJ
         free_area_repr (ifilter l) start end l SEPCONJ
+        free_area_head_repr (ifilter l) start end dl SEPCONJ
         store_pageinfo_array vmemmap_ptr start end l SEPAND
         PURE (
-            dlist_node pool_ptr l hl start end l /\ 
-            dlist_head pool_ptr l 0 max_order hl /\ 
-            (LENGTH l = len))
+            dlist_node pool_ptr (ifilter l) l dl hl start end dl /\ 
+            dlist_head pool_ptr l dl 0 max_order hl /\ 
+            (LENGTH l = len) /\
+            (LENGTH dl = len) /\
+            (LENGTH hl = max_order))
     )`;;
 
 (* TODO : 页面越界判断 *)
